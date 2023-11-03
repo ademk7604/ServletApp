743db9db9a7d78b5676422250bc280d1b3f7578d
adem kok <ademk7604@gmail.com>
2023-11-03 11:41:17 +0100

    servlet ein get request beispiel

---
+++ src/main/java/konu/notation/StartUp.java
@@ -0,0 +1,70 @@
+package konu.notation;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/anasayfa"})
+public class StartUp extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest reg, HttpServletResponse resp) throws IOException {
+		PrintWriter writer = resp.getWriter();
+		resp.setCharacterEncoding("UTF-8"); // tum karakterlere uyum saglamis olacak
+		resp.setHeader("content-type", "text/html;charset=UTF-8");
+		
+		writer.write("<h1>HOSGELDINIZ</h1>");
+		
+		List<String> takimlar = new ArrayList<String>() {
+			{
+				add("Galatasaray");
+				add("Fenerbahce");
+				add("Besiktas");
+				add("Trabzonspor");
+			}
+		};
+		
+		ArrayList<String> renkler = new ArrayList<>() {
+			{
+				add("red");
+				add("blue");
+				add("black");
+				add("green");
+			}
+		};
+		
+		
+		writer.write("<table>");
+		writer.write("<tr>");
+		writer.write("<th>");
+		writer.write("Takimlar");
+		writer.write("</th>");
+		writer.write("</tr>");
+		
+		String[] stilRenkleri = new String[renkler.size()];
+		stilRenkleri = renkler.toArray(stilRenkleri);
+		
+		int index = 0;
+		
+		for(String takim: takimlar) {
+			writer.write("<tr>");
+			writer.write("<td style='color:"+stilRenkleri[index++]+";'>");
+			writer.write(takim);
+			writer.write("</td>");
+			writer.write("</tr>");
+		}
+		
+		writer.write("</table>");
+		
+		
+		
+	}
+
+}
--- src/main/java/konuGiris/StartUp.java
+++ src/main/java/konuGiris/StartUp.java
@@ -1,5 +1,30 @@
 package konuGiris;
 
-public class StartUp {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class StartUp extends HttpServlet{
+	// client ile server arasindaki iletisimi sagliyor
+	//2. Servlet kaynak kodunun olustur.
+	//2. web.xml icerisinde mapliyoruz
+	//3. Apache Tomcat sunucusunu baslatiyoruz
+	//4. web tarayisini acip Servlet imize istekte bulunuyoruz.
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTP-8");
+		resp.setHeader("content-type", "text/html;UTF-8");
+		
+		PrintWriter printWriter = resp.getWriter();
+		String str = "Merhabalar, Hos Geldiniz!";
+		printWriter.write(str);
+		
+		
+	}
 
 }
